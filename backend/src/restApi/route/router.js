2664571048199aa3b9fcdb17a361d82939bfad84
gyudisanyi <gyudisanyi@gmail.com>
2021-05-24 14:08:10 +0200

    routes and controllers copied and altered from students to fit projects.

--- backend/src/restApi/route/router.js
+++ backend/src/restApi/route/router.js
@@ -1,10 +1,14 @@
 import express from 'express'
 const router = express()
 import student from './student'
+import project from './project'
 
 // Documentation
 // https://expressjs.com/en/api.html#router
 
 // Hallgatókat kezelő útvonalak
 router.use('/student', student)
+
+// Projekteket kezelő útvonalak
+router.use('/project', project)
 export default router
